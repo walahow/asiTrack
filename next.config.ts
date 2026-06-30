bf145836cb7cfe357596c3ee40ed9913a1169b2c
WALAHO <latifhamzah106@gmail.com>
2026-06-30 23:53:58 +0700

    storages blob

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.youtube.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
     ],
   },
 };
